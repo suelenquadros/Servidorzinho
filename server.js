e6074c2898f0e5543bfa0f30ad9a1853aab72252
suelenquadros <suelen.quadros17@gmail.com>
2019-05-30 22:30:09 -0300

    codigo metodo post

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const controller = require('./ComidasController')
 
 
 // import express from 'express'
 
 
-
-
-
-
 const servidor = express()
 servidor.use(cors())
 
-servidor.get("/comidas", (request,response)=> {
+servidor.get("/comidas", (request, response) => {
   // response.header('Access-Control-Allow-Origin','*')
   response.send(controller.getAll())
 
 })
 
+servidor.post('/comidas', bodyParser.json(), (request,
+  response) => {
+  controller.add(request.body)
+  response.send(201)
+
+})
 
 
 servidor.listen(3000)
