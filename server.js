5291252964105c93c7b6bb2f55715e2644e7829a
suelenquadros <suelen.quadros17@gmail.com>
2019-05-30 21:52:38 -0300

    instalacao pacote cors e alteracao rotas server

--- server.js
+++ server.js
@@ -1,49 +1,26 @@
 
-const http = require('http')
-const comidas = {
-    pratosFavoritos:
-    [
-    {
-      nome: "Batata frita",
-      descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-      imagem: "img/Batata-frita.jpg"
-    },
-    {
-      nome: "Macarronada",
-      descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-      imagem: "img/macarronada.jpg"
-    },
-    {
-      nome: "Falafel",
-      descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-      imagem: "img/falafel.jpg"
-    },
-    {
-      nome: "Creme de abóbora",
-      descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-      imagem: "img/creme-de-abobora.jpg"
-    },
-  
-  ]}
+const express = require('express')
+const cors = require('cors')
+const controller = require('./ComidasController')
 
-const servidor = http.createServer(function (request, response) {
-    if (request.url === '/') {
-        response.end('Hello wonderful world!')
-    } else if (request.url === '/comidas') {
-        if (request.method === 'GET') {
-            response.writeHead(200, {
-                "Content-Type": "application/json",
-                'Access-Control-Allow-Origin':"*"
-            })
-            response.write(JSON.stringify(comidas))
-            response.end()
-    } else if (request.method === 'POST') {
-        response.writeHead(200, {
-            "Content-Type": "text/html;charset=utf-8"
-        })
-        response.end("<h1>Respostão diferentona</h1>")
-        }
-    }
+
+// import express from 'express'
+
+
+
+
+
+
+const servidor = express()
+servidor.use(cors())
+
+servidor.get("/comidas", (request,response)=> {
+  // response.header('Access-Control-Allow-Origin','*')
+  response.send(controller.getAll())
+
 })
+
+
+
 servidor.listen(3000)
 console.log("servidor wonderful rodando na porta 3000") 
